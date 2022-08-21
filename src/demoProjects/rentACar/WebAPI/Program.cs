b4b011904ff0fc54fff89e0ff9a5641c53b87ad4
Umit Beçet <umtbecet@gmail.com>
2022-08-21 16:18:16 +0300

    21.08.2022-1

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddHttpContextAccessor();
 
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
