7721a96cc01fa0f17ddd3360f62df8b7e9f307be
Umit Beçet <umtbecet@gmail.com>
2022-09-11 12:46:08 +0300

    11.09.2022-1

--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -2,24 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Brand :Entity
+    public class Brand : Entity
     {
-        
         public string Name { get; set; }
+
+        public virtual ICollection<Model> Models { get; set; }
 
         public Brand()
         {
         }
 
-        public Brand(int id, string name)
+        public Brand(int id, string name) : this()
         {
-            Id= id;
+            Id = id;
             Name = name;
         }
     }
