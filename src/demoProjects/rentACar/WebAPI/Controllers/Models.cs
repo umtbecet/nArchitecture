6d5505af2d8aa73780293895998c197586b40689
Umit Beçet <umtbecet@gmail.com>
2022-09-11 14:37:10 +0300

    11.09.2022-2

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/Models.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetListModel;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Models : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest };
+            ModelListModel result = await Mediator.Send(getListModelQuery);
+            return Ok(result);
+        }
+
+    }
+}
